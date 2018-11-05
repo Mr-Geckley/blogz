383d9722d7bf102700a19821294cada68cf74138
Geckley <techncially.geckley@gmail.com>
2018-11-05 10:25:31 -0600

    figuring out how to display title and blog under w/i same html element

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:letsgo@localhost:8889/build-a-blog'
+                                         
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
 
-tasks = []   
+class Blog(db.Model): 
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(500))
+
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+blogs = []
+bodies = []   
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
     if request.method == 'POST':
-        task = request.form['task']
-        tasks.append(task)
+        blog = (request.form['blog'])
+        body = (request.form['body'])
 
-
+        blogs.append(blog)
+        bodies.append(body)
     
-    return render_template('todos.html', title="Build A Blog", tasks=tasks) 
+    return render_template('blog.html', title="Build A Blog", blogs=blogs, bodies=bodies)
 
-app.run()
+if __name__ == '__main__':
+    app.run()
